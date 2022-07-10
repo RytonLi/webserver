6092815fa03c1ea28c14a3920b12f31ba2c722ba
Rytonli <rytonli@163.com>
2022-07-10 15:09:26 +0800

    init

---
+++ epoll_server.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+    > File Name: epoll_server.h
+    > Author: Rytonli
+    > Mail: rytonli@163.com
+    > Created Time: Sun 10 Jul 2022 03:08:56 PM CST
+ ************************************************************************/
+
+#ifndef EPOLL_SERVER_H
+#define EPOLL_SERVER_H
+
+#endif
---
+++ main.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author: Rytonli
+    > Mail: rytonli@163.com
+    > Created Time: Sun 10 Jul 2022 03:08:53 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<pthread.h>
+
+int main(int argc, char* argv[]){
+
+  return 0;
+}
