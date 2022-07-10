f593e78ef7f40946724c09bb2304df09390ef3e1
Rytonli <rytonli@163.com>
2022-07-10 15:42:29 +0800

    ready

--- main.c
+++ main.c
@@ -5,13 +5,31 @@
     > Created Time: Sun 10 Jul 2022 03:08:53 PM CST
  ************************************************************************/
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "epoll_server.h"
 
 int main(int argc, char* argv[]){
+	if(argc < 3)
+    {
+        printf("eg: ./a.out port path\n");
+        exit(1);
+    }
+
+    // 采用指定的端口
+    int port = atoi(argv[1]);
+
+    // 修改进程工作目录, 方便后续操作
+    int ret = chdir(argv[2]);
+    if(ret == -1)
+    {
+        perror("chdir error");
+        exit(1);
+    }
+    
+    // 启动epoll模型 
+    epoll_run(port);
 
   return 0;
 }
